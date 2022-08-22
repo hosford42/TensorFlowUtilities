3cb5f3dd4b7abea00b919db5ba9b5984db908a93
Aaron Hosford <hosford42@gmail.com>
2022-08-21 20:29:28 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='TensorFlowUtilities',
+    version='0.0',
+    description='Assorted useful tensorflow functions and keras layers',
+    author='Aaron Hosford',
+    author_email='hosford42@gmail.com',
+    url='https://github.com/hosford42/TensorFlowUtilities',
+    packages=['tf_utilities'],
+)
